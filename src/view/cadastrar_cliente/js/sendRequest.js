5cb2961d682d9354d0297fc58a90921aa6d8fcdc
Bruno Ferreira <brunoferreira1999@live.com>
2021-09-28 19:56:23 -0300

    Integração do cadastro com backend concluida

--- src/view/cadastrar_cliente/js/sendRequest.js
+++ src/view/cadastrar_cliente/js/sendRequest.js
@@ -1,47 +1,106 @@
 const btnCadastrar = document.querySelector('#btn-cadastrar');
 
-btnCadastrar.addEventListener('click', async (evt) => {
+function validateAllFields () {
 
-   evt.preventDefault();
+   const txtNome = document.querySelector('#nome').value;
+   const txtCelular = document.querySelector('#celular').value;
+   const txtEmail = document.querySelector('#email').value;
+   const txtCep = document.querySelector('#cep').value;
+   const txtEndereco = document.querySelector('#endereco').value;
+   const txtNumero = document.querySelector('#numero').value;
+   const txtBairro = document.querySelector('#bairro').value;
+   const txtCidade = document.querySelector('#cidade').value;
+   const txtEstado = document.querySelector('#estado').value;
 
-   //Verificação dos campos de senha
+   if ( txtNome !== '' && txtCelular !== '' && txtEmail !== '' &&
+      txtCep !== '' && txtEndereco !== '' && txtNumero !== '' &&
+      txtBairro !== '' && txtCidade !== '' && txtEstado !== '' ){
+         return true;
+      }
+
+   return false;
+}
+
+function validatePassword () {
+
    const txtPassword = document.querySelector('#senha').value;
    const txtPasswordConfirm = document.querySelector('#confirmSenha').value;
 
    if (txtPassword.length < 6 || txtPasswordConfirm.length < 6)
-      return console.error('A senha deve ter pelo menos 6 caracteres')
+      return false;
 
    if (txtPassword !== txtPasswordConfirm)
-      return console.error('As senhas devem ser iguais');
+      return false;
 
-   //Preciso formatar o CEP para enviar ao banco sem hífen
-   const  cepFormatted = document.querySelector('#cep').value.replace(/-(?!>)/g, '');
-   
-   const userData = {
+   return true;
+}
 
-      name: document.querySelector('#nome').value,
-      email: document.querySelector('#email').value,
-      password: document.querySelector('#senha').value,
-      phone: document.querySelector('#celular').value,
-      cep: cepFormatted,
-      addressNumber: document.querySelector('#numero').value,
-   };
-   
-   console.log(userData);
+btnCadastrar.addEventListener('click', async (evt) => {
 
-   const fetchParams = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ userData }),
+   evt.preventDefault();
+
+   if (validatePassword() && validateAllFields()){
+
+      //Formatação do campo CEP para retirar o hífen antes de enviar ao backend 
+      const cepFormatted = document.querySelector('#cep').value.replace(/-(?!>)/g, '');
+      const phoneFormatted = document.querySelector('#celular').value.replace(/[^\d]+/g,'');
+
+      
+      const userData = {
+
+         name: document.querySelector('#nome').value,
+         email: document.querySelector('#email').value,
+         password: document.querySelector('#senha').value,
+         phone: phoneFormatted,
+         cep: cepFormatted,
+         addressNumber: document.querySelector('#numero').value,
+      };
+
+      const fetchParams = {
+         method: 'POST',
+         headers: { 
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({ userData }),
+      }
+
+      try {
+
+         console.log(fetchParams)
+         
+         const fetching = await fetch('http://localhost:3000/user-registration', fetchParams);
+         const fetchRes = await fetching.json();
+
+         console.log(fetchRes);
+
+         if (fetchRes.error) {
+            return Swal.fire({
+               icon: "error",
+               title: "Opa..",
+               text: fetchRes.error,
+               showConfirmButton: false,
+               timer: 2000,
+            });
+         }
+
+         Swal.fire({
+            icon: "success",
+            text: fetchRes.user.name + ', conta cadastrada com sucesso!',
+            showConfirmButton: false,
+            timer: 2000,
+         });
+
+         setTimeout(() => {
+            window.location.replace('https://livrados.vercel.app/view/login/');
+         }, 2200);
+
+      } catch (err) {
+         console.error("Error while trying to fetch to backend: " + err );
+      }
    }
 
-   try {
-      
-      const fetch = await fetch('https://localhost:3000/user-registration', fetchParams);
-      const fetchRes = fetch.json();
-
-   } catch (err) {
-      console.error("Error while trying to fetch to backend: " + err);
+   else{
+      console.error('Passwords do not match or blank fields');
    }
    
 })
